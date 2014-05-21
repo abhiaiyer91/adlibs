81036d93bc5ea0d8cb5868ddb73fda3ea35293ce
Abhi Aiyer <abhiaiyer91@gmail.com>
2014-05-20 22:07:02 -0700

    Controller + Refactoring + Gender Additions

--- scripts/controllers/main.js
+++ scripts/controllers/main.js
@@ -2,9 +2,46 @@
 
 angular.module('adlibsApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    $scope.adlib = 
+    	{
+    		name: 'Abhi Aiyer',
+    		dirty: 'Dirty Task',
+    		obnox: 'Kanye West',
+    		job: 'Web Developer',
+    		celebrity: 'George Clooney',
+    		bignumber: 0,
+    		tedious: 'Padding & Margins',
+    		useless: 'Basket Weaving',
+    		adj: 'Gigantic'
+
+    	};
+
+    $scope.gender = 
+    	{
+    		male: 
+    		{
+    			valid: false,
+    			his: 'his',
+    			him: 'him',
+    			he: 'he'
+    		},
+    		female: 
+    		{
+    			valid: false,
+    			her: 'her',
+    			she: 'she'  			
+    		}
+    		
+    	}
+
+   $scope.clickMale = function (){
+   		$scope.gender.male.valid = true;
+   		$scope.gender.female.valid = false;
+   };
+
+   $scope.clickFemale = function (){
+   		$scope.gender.male.valid = false;
+   		$scope.gender.female.valid = true;
+   };
+
   });
