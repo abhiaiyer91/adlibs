33af7a221fc090a402a7adf3f469aaf49448ae33
Abhi Aiyer <abhiaiyer91@gmail.com>
2014-05-20 20:23:11 -0700

    Initial dist subtree commit

---
+++ dist/scripts/ca68d010.scripts.js
@@ -0,0 +1,1 @@
+"use strict";angular.module("adlibsApp",["ngRoute"]).config(["$routeProvider",function(a){a.when("/",{templateUrl:"views/main.html",controller:"MainCtrl"}).otherwise({redirectTo:"/"})}]),angular.module("adlibsApp").controller("MainCtrl",["$scope",function(a){a.awesomeThings=["HTML5 Boilerplate","AngularJS","Karma"]}]);
